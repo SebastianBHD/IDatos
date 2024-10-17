a8d8d5027c7f7cb37c19ecf8152aa771cf60ce42
sebastian.britos <sebastian.britos.peluffo@gmail.com>
2024-10-16 21:31:13 -0300

    inicios de la pagina

---
+++ backend/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    database: 'books',
+    user: 'root',
+    password: 'Valeseba07'
+});
+
+conexion.connect(function(error) {
+    if (error) {
+        console.error('Error de conexión: ', error);
+        return;
+    } 
+    console.log('Conexión exitosa a la base de datos.');
+});
+
+app.get('/api/books', (req, res) => {
+    conexion.query('SELECT * FROM bookstable1', function(error, results, fields) {
+        if (error) {
+            console.error('Error en la consulta: ', error);
+            return res.status(500).json({ error: 'Error en la consulta' });
+        }
+        res.json(results);
+    });
+});
+
+app.get('/api/books/search', (req, res) => {
+    const searchTerm = req.query.search || '';
+    console.log('Término:', searchTerm); 
+    const query = `SELECT * FROM bookstable1 WHERE book_title LIKE ?`; 
+
+    conexion.query(query, [`%${searchTerm}%`], function(error, results) {
+        if (error) {
+            console.error('Error en la consulta: ', error);
+            return res.status(500).json({ error: 'Error en la consulta' });
+        }
+        res.json(results);
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
---
+++ frontend/js/app.js
@@ -0,0 +1,100 @@
+allBooks = [];
+
+
+function loadBooks() {
+    fetch('http://localhost:3000/api/books')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al obtener los datos');
+            }
+            return response.json();
+        })
+        .then(data => {
+            allBooks = data;
+        })
+        .catch(error => {
+            console.error('Error al obtener los datos:', error);
+        });
+}
+
+function filterBooks() {
+    const searchTerm = document.getElementById('searchBar').value;
+    console.log('Término de búsqueda:', searchTerm);
+    fetch(`http://localhost:3000/api/books/search?search=${searchTerm}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al obtener los datos');
+            }
+            return response.json();
+        })
+        .then(data => {
+            allBooks = data;
+            displayBooks(allBooks);
+        })
+        .catch(error => {
+            console.error('Error al obtener los datos:', error);
+        });
+}
+
+function displayBooks(books) {
+    const bookGrid = document.getElementById('bookGrid');
+    bookGrid.innerHTML = '';
+
+    books.forEach(book => {
+
+        const bookElement = document.createElement('div');
+        bookElement.className = 'book-card';
+        const imageUrls = [book.image_url_l, book.image_url_m, book.image_url_s];
+
+        const checkImageSize = (urlIndex) => {
+            if (urlIndex >= imageUrls.length) {
+                let bookImage = 'https://via.placeholder.com/200x300/cccccc/ffffff?text=No+Image';
+                return bookImage;
+            }
+
+            const img = new Image();
+            img.src = imageUrls[urlIndex] || ''; 
+
+            return new Promise((resolve) => {
+                img.onload = () => {
+                    if (img.naturalWidth >= 50 && img.naturalHeight >= 50) {
+                        resolve(imageUrls[urlIndex]);
+                    } else {
+                        resolve(checkImageSize(urlIndex + 1));
+                    }
+                };
+
+                img.onerror = () => {
+                    resolve(checkImageSize(urlIndex + 1)); 
+                };
+            });
+        };
+
+        checkImageSize(0).then((validImageUrl) => {
+
+            const imgElement = document.createElement('img');
+            imgElement.src = validImageUrl;
+            imgElement.alt = book.book_title;
+
+            bookElement.innerHTML = `
+                <div class="book-info">
+                <h3>${book.book_title}</h3>
+                <p>Autor: ${book.book_author}</p>
+                <p>Año: ${book.year_of_publication}</p>
+                <p>Editorial: ${book.publisher}</p>
+                </div>
+            `;
+
+            bookElement.insertBefore(imgElement, bookElement.firstChild);
+            bookGrid.appendChild(bookElement);
+        });
+    });
+}
+
+
+function loadMoreBooks() {
+    loadBooks();
+}
+function loadFilterBooks() {
+    filterBooks();
+}
